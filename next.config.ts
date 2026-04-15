380eb37c28662991b3472a9cbd3ae7c4118afeac
doruktoto <148455552+doruktoto@users.noreply.github.com>
2026-04-15 23:44:17 +0100

    Build Again

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': ['content/**/*'],
-    },
+  outputFileTracingExcludes: {
+    '*': ['content/**/*'],
   },
 };
 
