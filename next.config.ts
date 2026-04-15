78e93884a9d44ae0cf3d2e45982910e756696596
doruktoto <148455552+doruktoto@users.noreply.github.com>
2026-04-15 23:22:01 +0100

    Initial Portfolio

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['content/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
